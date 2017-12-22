d2cbc47783987c88f37beaa8cd101e6b19c4169c
Richard <droste.richard@gmail.com>
2017-12-22 20:30:09 +0000

    Cleaned up day 1 and 3

--- 003_spiral.py
+++ 003_spiral.py
@@ -1,37 +1,23 @@
 import math
 
 n = 368078
-# n=28
-# n=25
-
-# for i in range(2,n):
 
 
-layer_end = lambda l: (2*l+1)**2
-# layer_n = layer_end - (2*l-2)^2
+def layer_end(l):
+    return (2*l+1)**2
+
 
 l = math.ceil((math.sqrt(n)-1)/2)
 ple = layer_end(l-1)
-# le = layer_end(l)
-# nl = le - ple
 ns = 2*l
 rn = n-ple
-
-print(l)
-print(ple)
-# print(le)
-# print(nl)
-print(4*ns)
-
-# row =
-# col =
 
 if rn >= 3*ns:
     col = rn - 3*ns - l
     row = -l
 elif rn > 2*ns:
     col = -l
-    row = 2*ns - rn + l 
+    row = 2*ns - rn + l
 elif rn >= ns:
     col = ns - rn + l
     row = l
