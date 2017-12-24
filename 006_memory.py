e71be801aceb8d3f8ccb2efcc3b076624a2f0fae
Richard <droste.richard@gmail.com>
2017-12-24 02:37:38 +0100

    Improved solution to day 6

--- 006_memory.py
+++ 006_memory.py
@@ -17,21 +17,14 @@ mem_size = len(mem)
 hashes = [myhash(mem)]
 counter = 0
 while True:
-    # print(mem)
-    counter = counter + 1
+    counter += 1
 
     pos = np.argmax(mem)
     val = mem[pos]
     mem[pos] = 0
-    # print(mem)
 
     for i in range(val):
-        if pos < mem_size - 1:
-            pos = pos + 1
-        else:
-            pos = 0
-
-        mem[pos] = mem[pos] + 1
+        mem[(pos + 1 + i) % (mem_size)] += 1
 
     this_hash = myhash(mem)
     if this_hash in hashes:
