97680c0a1cc8e4dc590efb14c637e207c03d4c5c
Richard <droste.richard@gmail.com>
2017-12-26 18:48:48 +0100

    Inproved 8

--- 008_registers.py
+++ 008_registers.py
@@ -2,7 +2,8 @@
 import operator
 
 ops = {">": operator.gt, ">=": operator.ge, "==": operator.eq,
-       "<": operator.lt, "<=": operator.le, "!=": operator.ne}
+       "<": operator.lt, "<=": operator.le, "!=": operator.ne,
+       "inc": operator.__iadd__, "dec": operator.__isub__}
 
 register = {}
 with open('008_input.txt') as f:
@@ -14,10 +15,6 @@ with open('008_input.txt') as f:
                 register[words[i]] = 0
 
         if ops[words[5]](register[words[4]], int(words[6])):
-            if words[1] == 'inc':
-                register[words[0]] += int(words[2])
-            if words[1] == 'dec':
-                register[words[0]] -= int(words[2])
+            register[words[0]] = ops[words[1]](register[words[0]], int(words[2]))
 
 print(register[max(register, key=register.get)])
-# print(register)
