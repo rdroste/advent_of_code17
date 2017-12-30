9e0d1ccacc7decb82e9e6754ad90a27c15e2312e
Richard <droste.richard@gmail.com>
2017-12-30 23:12:52 +0100

    Day 5, part 2

---
+++ aoc_005_maze_2.py
@@ -0,0 +1,24 @@
+
+
+with open('005_input.txt','r') as infile:
+    content = infile.readlines()
+maze = [int(s) for s in content]
+
+pos = 0
+size = len(maze)
+caught = True
+steps = 0
+while caught:
+
+    steps = steps + 1
+    jump = maze[pos]
+    if jump < 3:
+        maze[pos] += 1
+    else:
+        maze[pos] -= 1
+    pos = pos + jump
+
+    if pos < 0 or pos >= size:
+        caught = False
+
+print(steps)
