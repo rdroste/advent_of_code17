a66f1d78bed905139cb469af38831faacb55446e
Richard <droste.richard@gmail.com>
2017-12-30 15:57:53 +0100

    Misc

--- 010_hash.py
+++ 010_hash.py
@@ -3,7 +3,7 @@ from knothash import knothash
 
 with open('010_input.txt','r') as f:
     content = f.readline()
-    lengths = [int(x.strip()) for x in content.split(',')]
+    lengths = [int(x) for x in content.strip().split(',')]
 
 nums = knothash(lengths, list(range(0,256)), 0, 0)[0]
 
