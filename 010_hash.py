a5b909e48bcf26f12756b1b8af26fbbc17210baa
Richard <droste.richard@gmail.com>
2017-12-30 15:54:36 +0100

    Day 10, part 2

--- 010_hash.py
+++ 010_hash.py
@@ -1,23 +1,10 @@
 
+from knothash import knothash
+
 with open('010_input.txt','r') as f:
     content = f.readline()
     lengths = [int(x.strip()) for x in content.split(',')]
 
-n = 256
-nums = list(range(0,n))
-pos = 0
-skip = 0
-for l in lengths:
-
-    if l > n-1:
-        continue
-
-    for i in range(0, l//2):
-        x = (pos + i) % n
-        y = (pos + l - i - 1) % n
-        nums[x], nums[y] = nums[y], nums[x]
-
-    pos = (pos + l + skip) % n
-    skip += 1
+nums = knothash(lengths, list(range(0,256)), 0, 0)[0]
 
 print(nums[0]*nums[1])
---
+++ knothash.py
@@ -0,0 +1,18 @@
+
+
+def knothash(lengths, nums, pos, skip):
+    n = len(nums)
+    for l in lengths:
+
+        if l > n-1:
+            continue
+
+        for i in range(0, l//2):
+            x = (pos + i) % n
+            y = (pos + l - i - 1) % n
+            nums[x], nums[y] = nums[y], nums[x]
+
+        pos = (pos + l + skip) % n
+        skip += 1
+
+    return nums, pos, skip
