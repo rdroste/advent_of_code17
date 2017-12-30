2692531b33f6ebeb053c26bd961381ec3d113304
Richard <droste.richard@gmail.com>
2017-12-30 17:40:16 +0100

    Day 14

--- 010_hash-2.py
+++ 010_hash-2.py
@@ -1,23 +1,10 @@
 
 
-from knothash import knothash
+from knothash import knothash2
 from functools import reduce
+
 
 with open('010_input.txt','r') as f:
     content = f.readline().strip()
-# content = 'AoC 2017'
-lengths = [ord(c) for c in list(content)] + [17, 31, 73, 47, 23]
 
-sparse = list(range(0,256))
-pos = 0
-skip = 0
-for i in range(64):
-    sparse, pos, skip = knothash(lengths, sparse, pos, skip)
-
-dense = []
-for i in range(0,256,16):
-    dense.append(reduce(lambda x,y: x ^ y, sparse[i:i+16]))
-
-dense_hash = ''.join('{:02x}'.format(d) for d in dense)
-
-print(dense_hash)
+print(knothash2(content))
--- knothash.py
+++ knothash.py
@@ -1,4 +1,6 @@
 
+from functools import reduce
+
 
 def knothash(lengths, nums, pos, skip):
     n = len(nums)
@@ -16,3 +18,19 @@ def knothash(lengths, nums, pos, skip):
         skip += 1
 
     return nums, pos, skip
+
+
+def knothash2(content):
+    lengths = [ord(c) for c in list(content)] + [17, 31, 73, 47, 23]
+
+    sparse = list(range(0,256))
+    pos = 0
+    skip = 0
+    for i in range(64):
+        sparse, pos, skip = knothash(lengths, sparse, pos, skip)
+
+    dense = []
+    for i in range(0,256,16):
+        dense.append(reduce(lambda x,y: x ^ y, sparse[i:i+16]))
+
+    return ''.join('{:02x}'.format(d) for d in dense)
