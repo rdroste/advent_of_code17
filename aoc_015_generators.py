d309035022ff433c49622be3425a6003efb8f14f
Richard <droste.richard@gmail.com>
2017-12-31 11:41:35 +0100

    Day 15, part 1

--- aoc_015_generators.py
+++ aoc_015_generators.py
@@ -1,5 +1,14 @@
 
 
-a_start = 512
-b_start = 191
+val = [512, 191]
+factor = [16807, 48271]
+divisor = 2147483647
 
+matches = 0
+for i in range(int(40e6)):
+    val = [(v * f) % divisor for v,f in zip(val, factor)]
+    lower_bits = [v & 0xffff for v in val]
+    if lower_bits[0] == lower_bits[1]:
+        matches += 1
+
+print(matches)
