f85182e7c25b711ee5bc91502bd190a73ee0fd1d
Richard <droste.richard@gmail.com>
2017-12-31 16:52:31 +0100

    Misc

--- aoc_014_defrag.py
+++ aoc_014_defrag.py
@@ -6,7 +6,7 @@ result = 0
 for row in range(128):
     this_key = key + '-' + str(row)
     knot_hash = knothash2(this_key)
-    bin_hash = ''.join('{:04b}'.format(int(k, 16)) for k in knot_hash)
+    bin_hash = bin(int(knot_hash,16))[2:]
     result += bin_hash.count('1')
 
 print(result)
