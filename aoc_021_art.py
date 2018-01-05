250d5504057abd151516f2eac1933292e1f0d91e
Richard <droste.richard@gmail.com>
2018-01-05 22:34:44 +0100

    Day 21, part 2

--- aoc_021_art.py
+++ aoc_021_art.py
@@ -1,46 +1,30 @@
 
-
 import numpy as np
-import matplotlib.pyplot as plt
 
 
 def art2mat(art):
     return np.asmatrix([[int(b=='#') for b in list(a)] for a in art.strip().split('/')])
 
 
-def checkkey(key, pattern):
-    if key.shape[0] == pattern.shape[0]:
-        for i in range(4):
-            key = np.rot90(key)
-            if (np.all(key == pattern) or np.all(np.fliplr(key) == pattern) or
-            np.all(np.flipud(key) == pattern)):
-                return True
-
-    return False
+def mat2tuple(mat):
+    return tuple(np.asarray(mat.flatten()).tolist()[0])
 
 
-def printart(grid):
-    lgrid = grid.tolist()
-    signs = {0:'.', 1:'#'}
-    art = ''
-    for i in range(grid.shape[0]):
-        art += ''.join([signs[j] for j in lgrid[i]]) + '\n'
-
-    print(art)
-
-
-rules = []
+rules = {}
 with open('021_input.txt','r') as f:
     for line in f:
         words = line.strip().split(' ')
         key = art2mat(words[0])
         val = art2mat(words[2])
-        rules.append((key, val))
+        for i in range(4):
+            key = np.rot90(key)
+            rules[mat2tuple(key)] = val
+            rules[mat2tuple(np.fliplr(key))] = val
+            rules[mat2tuple(np.flipud(key))] = val
 
 init = '.#./..#/###'
 
 grid = art2mat(init)
-sums = []
 for i in range(18):
 
     gsize = grid.shape[0]
@@ -50,14 +34,7 @@ for i in range(18):
     for x in range(pn):
         for y in range(pn):
             pattern = grid[x*psize:(x+1)*psize, y*psize:(y+1)*psize]
-            new_pattern = None
-            for key_, val_ in rules:
-                if checkkey(key_, pattern):
-                    new_pattern = val_
-                    break
-
-            if new_pattern is None:
-                print('No pattern found')
+            new_pattern = rules[mat2tuple(pattern)]
 
             if y == 0:
                 new_row = new_pattern.copy()
@@ -70,12 +47,7 @@ for i in range(18):
             new_grid = np.concatenate((new_grid, new_row), axis=0)
 
     grid = new_grid
-    # printart(grid)
-    print(np.sum(grid))
-    print(i+1)
-    sums.append(np.sum(grid))
+    if i == 4:
+        print(np.sum(grid))
 
-
-print(sums)
-# plt.plot(sums)
-# plt.show()
+print(np.sum(grid))
