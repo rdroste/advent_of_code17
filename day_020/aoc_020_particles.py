e690c466b1f1cb281279797cd9ed7272871c9741
Richard <droste.richard@gmail.com>
2018-01-06 18:34:56 +0100

    Day 20, part 1

--- day_020/aoc_020_particles.py
+++ day_020/aoc_020_particles.py
@@ -1,15 +1,24 @@
 
 
-max_accel = 0
-part = 0
+max_accel = []
 with open('020_input.txt') as f:
     for i, line in enumerate(f):
         words = line.strip().split(' ')
         accels = [abs(int(a)) for a in words[2][3:-1].split(',')]
-        this_max_accel = sum(accels)
-        # print(this_max_accel)
-        if this_max_accel > max_accel:
-            part = i
-            max_accel = this_max_accel
+        max_accel.append(max(accels))
 
-print(part)
+minima = [i for i,a in enumerate(max_accel) if a == min(max_accel)]
+print(minima)
+
+# max_velo = []
+# if len(minima) > 1:
+#     with open('020_input.txt') as f:
+#         for i, line in enumerate(f):
+#             if i in minima:
+#                 words = line.strip().split(' ')
+#                 print(words)
+#                 velo = [abs(int(v)) for v in words[1][3:-2].split(',')]
+#                 max_velo.append(max(velo))
+#     print(minima[max_velo.index(min(max_velo))])
+# else:
+#     print(minima[0])
